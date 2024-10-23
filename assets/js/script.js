fc864c1add7b013b386fbc34722620b6457ff160
Alessandro Mendes <165946341+AlessandroMendesS@users.noreply.github.com>
2024-10-23 00:41:23 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+   // Inicialização do Typed.js
+   new Typed('#texto-digitado', {
+    strings: [
+        'Análises aprofundadas',
+        'Guias completos',
+        'Notícias exclusivas',
+        'Conteúdo de qualidade'
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 2000,
+    loop: true
+});
