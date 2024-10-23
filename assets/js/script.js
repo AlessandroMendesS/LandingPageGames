b4ab4187f59f689c58b05a6554a8f4ad2ac331bf
Alessandro Mendes <165946341+AlessandroMendesS@users.noreply.github.com>
2024-10-23 09:59:03 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+   new Typed('#texto-digitado', {
+    strings: [
+        'Análises aprofundadas',
+        'Guias completos',
+        'Notícias exclusivas',
+        'Conteúdo de qualidade'
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 2000,
+    loop: true
+});
